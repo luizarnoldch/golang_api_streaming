df0bde4cb0c10210dd1477f74c4b8508c3f18c4a
luizarnoldch <luizarnoldch@gmail.com>
2024-01-05 13:18:08 -0500

    functional testing binary files

--- scripts/func_test.sh
+++ scripts/func_test.sh
@@ -6,7 +6,7 @@ test_lambda() {
   for folder in "${FOLDERS[@]}"; do
     (
       folder_name=$(basename "${folder}")
-      aws lambda invoke --function-name StreamProyect-${folder_name} --payload file://events/${folder_name}/input.json --cli-binary-format raw-in-base64-out ./events/${folder_name}/output.json
+      aws lambda invoke --function-name StreamProyect-${folder_name} --payload fileb://events/${folder_name}/input.json --cli-binary-format raw-in-base64-out ./events/${folder_name}/output.json
       echo -e "\n"
       cat ./events/${folder_name}/output.json
       echo -e "\n"
